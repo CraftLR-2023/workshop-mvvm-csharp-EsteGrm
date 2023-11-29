dd12afc8ae29735eb135fbec684d18f1248ebec6
EsteGrm <esteban.girma@viacesi.fr>
2023-11-29 15:20:18 +0100

    exo1a4

--- src/CraftLR.Exercice3/HelloBeautifulButton.axml.cs
+++ src/CraftLR.Exercice3/HelloBeautifulButton.axml.cs
@@ -2,6 +2,7 @@ using System;
 
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging; // Assurez-vous d'importer cette directive
 
 namespace CraftLR.Exercice3;
 
@@ -10,5 +11,13 @@ public partial class HelloBeautifulButton : Window
     public HelloBeautifulButton()
     {
         InitializeComponent();
+        LoadImage();
+    }
+
+    private void LoadImage()
+    {
+            var image = new Bitmap("Assets/logo.png");
+            var button = this.FindControl<Button>("Bouton1");
+            button.Content = new Image { Source = image };
     }
 }
